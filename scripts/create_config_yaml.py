d31ef04eae242d8b0c609d52470b82204512a81f
Anton Potapov <potapov.a.p13@gmail.com>
2023-12-15 00:55:19 +0000

    Make jobs dynamic for parallel images building

---
+++ scripts/create_config_yaml.py
@@ -0,0 +1,118 @@
+import yaml
+from model.godot_release_entity import GodotRelease
+
+
+CIRCLE_CI_CONFIG_VERSION = 2.1
+CIRCLE_CI_JOB_IMAGE = "cimg/base:stable"
+CIRCLE_CI_REMOTE_DOCKER_VERSION = "20.10.18"
+
+# Docker file to build an image with
+DOCKER_FILE = "images/godot.dockerfile"
+# https://hub.docker.com/repository/docker/flashlight13/godot
+DOCKER_NAMESPACE = "flashlight13"
+DOCKER_REPOSITORY = "godot"
+# Setup in CI
+DOCKER_LOGIN_ENV_CONST = "DOCKER_LOGIN"
+DOCKER_PASS_ENV_CONST = "DOCKER_PASSWORD"
+# Version of Ubuntu to use for the image
+UBUNTU_VERSION = "23.04"
+
+
+def create(releases, output_file):
+    json_config = config_template()
+    if releases:
+        releases_log = []
+        for release in releases:
+            releases_log.append(release.printable_version())
+            job_name = "publish-" + \
+                release.printable_version().replace('.', "_")
+            json_config["jobs"][job_name] = job_template(
+                steps_for_release(release))
+            json_config["workflows"]["publish"]["jobs"].append(job_name)
+        print("Included in the config: " + ", ".join(releases_log))
+    else:
+        job_name = "empty_job"
+        json_config["jobs"][job_name] = job_template(
+            [
+                {
+                    "run": "echo nothing to do here"
+                },
+            ]
+        )
+        json_config["workflows"]["publish"]["jobs"].append(job_name)
+        print("Nothing to include. An empty config created")
+    yaml.safe_dump(
+        json_config,
+        output_file,
+        indent=2,
+        default_style=None,
+        default_flow_style=False
+    )
+
+
+def config_template():
+    return {
+        "version": CIRCLE_CI_CONFIG_VERSION,
+        "jobs": {
+
+        },
+        "workflows": {
+            "publish": {
+                "jobs": [],
+            },
+        },
+    }
+
+
+def job_template(steps):
+    return {
+        "docker": [
+            {"image": CIRCLE_CI_JOB_IMAGE},
+        ],
+        "steps": steps,
+    }
+
+
+def steps_for_release(release):
+    docker_tag = DOCKER_NAMESPACE + "/" + DOCKER_REPOSITORY + \
+        ":" + release.version + "-" + release.channel
+    return [
+        # checkout code
+        "checkout",
+        {
+            "setup_remote_docker": {
+                "version": CIRCLE_CI_REMOTE_DOCKER_VERSION
+            }
+        },
+
+        # Login to the Docker
+        {
+            "run": "echo " + DOCKER_PASS_ENV_CONST + " | docker login - u "
+            + DOCKER_LOGIN_ENV_CONST + " --password-stdin",
+        },
+
+        # Build image
+        {
+            "run": "docker build" + " "
+            + "--tag " + docker_tag + " "
+            + "--build-arg ubuntuVersion=" + UBUNTU_VERSION + " "
+            + "--build-arg godotVersion=" + release.version + " "
+
+            + "--build-arg engineUrl=" + release.engine_url + " "
+            + "--build-arg engineArchiveName=" + release.engine_archive_name + " "
+            + "--build-arg engineFileName=" + release.engine_file_name + " "
+
+            + "--build-arg templatesUrl=" + release.templates_url + " "
+            + "--build-arg templatesArchiveName=" + release.templates_archive_name + " "
+            + "--build-arg templatesDirectory=" + \
+            release.version + "." + release.channel + " "
+
+            + "-f " + DOCKER_FILE + " "
+            + "."
+        },
+
+        # Push the image
+        {
+            "run": "docker push " + docker_tag
+        },
+    ]
--- scripts/docker.py
+++ scripts/docker.py
@@ -1,75 +1,31 @@
 import requests
-import subprocess
 import json
 
-# Page sisze when loading existing versions from the Docker
 PAGE_SIZE = 100
+DEBUG_PAGE_SIZE = 5
 
-DOCKER_FILE = "images/godot.dockerfile"
-
-# https://hub.docker.com/repository/docker/flashlight13/godot 
+# https://hub.docker.com/repository/docker/flashlight13/godot
 DOCKER_NAMESPACE = "flashlight13"
 DOCKER_REPOSITORY = "godot"
 
-# Version of Ubuntu to use for the image
-UBUNTU_VERSION = "23.04"
 
+def load_existing_versions(debug):
+    if debug:
+        page_size = DEBUG_PAGE_SIZE
+    else:
+        page_size = PAGE_SIZE
+    existing_versions = []
+    url = "https://hub.docker.com/v2/namespaces/" + DOCKER_NAMESPACE + \
+        "/repositories/" + DOCKER_REPOSITORY + \
+        "/tags" + "?page_size=" + str(page_size)
+    while url:
+        response = requests.get(url)
+        response = json.loads(response.content)
+        existing_versions += map(
+            lambda result: result["name"], response["results"])
+        url = response["next"]
+        # load only the first page for debugging
+        if debug:
+            return existing_versions
 
-def login(args):
-	print("Logging in to Docker")
-	result = subprocess.run(
-		"echo " + args.docker_pass + " | docker login -u " + args.docker_login + " --password-stdin",
-		shell = True,
-		stderr = subprocess.PIPE,
-		check = True,
-	)
-
-
-def load_existing_versions():
-	existing_versions = []
-	url = "https://hub.docker.com/v2/namespaces/" + DOCKER_NAMESPACE + "/repositories/" + DOCKER_REPOSITORY + "/tags" + "?page_size=" + str(PAGE_SIZE)
-	while url:
-		response = requests.get(url)
-		response = json.loads(response.content)
-		existing_versions += map(lambda result: result["name"], response["results"])
-		url = response["next"]
-
-	return existing_versions
-
-
-def upload_docker(release, debug):
-	docker_tag = DOCKER_NAMESPACE + "/" + DOCKER_REPOSITORY + ":" + release.version + "-" + release.channel
-	_run_command(
-		[
-			'docker', 'build',
-			"--tag", docker_tag,
-			"--build-arg", "ubuntuVersion=" + UBUNTU_VERSION,
-			
-			"--build-arg", "godotVersion=" + release.version,
-			
-			"--build-arg", "engineUrl=" + release.engine_url,
-			"--build-arg", "engineArchiveName=" + release.engine_archive_name,
-			"--build-arg", "engineFileName=" + release.engine_file_name,
-
-			"--build-arg", "templatesUrl=" + release.templates_url,
-			"--build-arg", "templatesArchiveName=" + release.templates_archive_name,
-			"--build-arg", "templatesDirectory=" + release.version + "." + release.channel,
-
-			"-f", DOCKER_FILE,
-			".",
-		],
-		debug,
-		)
-
-	_run_command(['docker', 'push', docker_tag], debug)
-
-
-def _run_command(command, debug):
-	if debug:
-		print(' '.join(command))
-	else:
-		try:
-			result = subprocess.run(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, check = True)
-		except subprocess.CalledProcessError as exc:
-			print(exc.stderr.decode())
-			raise exc
+    return existing_versions
